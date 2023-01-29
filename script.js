495bf14af0e19d2f5bb9ee24185cc1221913f300
yuvraj <yuvrajsinha8334@gmail.com>
2023-01-29 09:48:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// var myList =[
+//     {
+//         "id":"bitcoin",
+//         "symbol":"btc",
+//         "name":"Bitcoin",
+//         "image":"https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+//         "current_price":23034,
+//         "market_cap":444577670338,
+//         "market_cap_rank":1,
+//         "fully_diluted_valuation":484416297303,
+//         "total_volume":44338946906,
+//         "high_24h":23501,"low_24h":22438,
+//         "price_change_24h":312.0,
+//         "price_change_percentage_24h":1.37313,
+//         "market_cap_change_24h":6704554068,
+//         "market_cap_change_percentage_24h":1.53116,
+//         "circulating_supply":19272950.0,
+//         "total_supply":21000000.0,
+//         "max_supply":21000000.0,
+//         "ath":69045,
+//         "ath_change_percentage":-66.5906,
+//         "ath_date":"2021-11-10T14:24:11.849Z",
+//         "atl":67.81,
+//         "atl_change_percentage":33918.26119,
+//         "atl_date":"2013-07-06T00:00:00.000Z",
+//         "roi":null,"last_updated":"2023-01-26T07:47:12.857Z"
+//     },
+//     { "id":"ethereum","symbol":"eth","name":"Ethereum","image":"https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880","current_price":1610.11,"market_cap":194120886735,"market_cap_rank":2,"fully_diluted_valuation":194120886735,"total_volume":12030992346,"high_24h":1630.13,"low_24h":1537.23,"price_change_24h":55.55,"price_change_percentage_24h":3.57309,"market_cap_change_24h":6601034696,"market_cap_change_percentage_24h":3.52018,"circulating_supply":120516982.275053,"total_supply":120516982.275053,"max_supply":null,"ath":4878.26,"ath_change_percentage":-66.98135,"ath_date":"2021-11-10T14:24:19.604Z","atl":0.432979,"atl_change_percentage":371912.54649,"atl_date":"2015-10-20T00:00:00.000Z","roi":{"times":92.40851847404126,"currency":"btc","percentage":9240.851847404127},"last_updated":"2023-01-26T07:47:18.006Z"},{"id":"tether","symbol":"usdt","name":"Tether","image":"https://assets.coingecko.com/coins/images/325/large/Tether.png?1668148663","current_price":1.002,"market_cap":67285345019,"market_cap_rank":3,"fully_diluted_valuation":67285345019,"total_volume":52991478560,"high_24h":1.008,"low_24h":0.990433,"price_change_24h":0.0026594,"price_change_percentage_24h":0.26599,"market_cap_change_24h":421037700,"market_cap_change_percentage_24h":0.62969,"circulating_supply":67188360789.139,"total_supply":67188360789.139,"max_supply":null,"ath":1.32,"ath_change_percentage":-24.31056,"ath_date":"2018-07-24T00:00:00.000Z","atl":0.572521,"atl_change_percentage":74.91821,"atl_date":"2015-03-02T00:00:00.000Z","roi":null,"last_updated":"2023-01-26T07:45:15.895Z"},{"id":"usd-coin","symbol":"usdc","name":"USD Coin","image":"https://assets.coingecko.com/coins/images/6319/large/USD_Coin_icon.png?1547042389","current_price":1.003,"market_cap":43696020020,"market_cap_rank":4,"fully_diluted_valuation":43696019986,"total_volume":3792349543,"high_24h":1.011,"low_24h":0.987271,"price_change_24h":0.00322466,"price_change_percentage_24h":0.32269,"market_cap_change_24h":-31992552.47608185,"market_cap_change_percentage_24h":-0.07316,"circulating_supply":43638296828.3734,"total_supply":43638296794.3734,"max_supply":null,"ath":1.17,"ath_change_percentage":-14.61435,"ath_date":"2019-05-08T00:40:28.300Z","atl":0.891848,"atl_change_percentage":12.27506,"atl_date":"2021-05-19T13:14:05.611Z","roi":null,"last_updated":"2023-01-26T07:47:53.338Z"},{"id":"binancecoin","symbol":"bnb","name":"BNB","image":"https://assets.coingecko.com/coins/images/825/large/bnb-icon2_2x.png?1644979850","current_price":306.87,"market_cap":41440084673,"market_cap_rank":5,"fully_diluted_valuation":61433456807,"total_volume":902821328,"high_24h":310.79,"low_24h":298.35,"price_change_24h":3.52,"price_change_percentage_24h":1.16099,"market_cap_change_24h":487593058,"market_cap_change_percentage_24h":1.19063,"circulating_supply":134910476.56,"total_supply":157219628.63,"max_supply":200000000.0,"ath":686.31,"ath_change_percentage":-55.22852,"ath_date":"2021-05-10T07:24:17.097Z","atl":0.0398177,"atl_change_percentage":771591.1214,"atl_date":"2017-10-19T00:00:00.000Z","roi":null,"last_updated":"2023-01-26T07:47:46.571Z"},{"id":"ripple","symbol":"xrp","name":"XRP","image":"https://assets.coingecko.com/coins/images/44/large/xrp-symbol-white-128.png?1605778731","current_price":0.412665,"market_cap":20951490059,"market_cap_rank":6,"fully_diluted_valuation":41245625780,"total_volume":1213286579,"high_24h":0.422068,"low_24h":0.404825,"price_change_24h":0.00309504,"price_change_percentage_24h":0.75568,"market_cap_change_24h":142030180,"market_cap_change_percentage_24h":0.68253,"circulating_supply":50796877639.0,"total_supply":99989148225.0,"max_supply":100000000000.0,"ath":3.4,"ath_change_percentage":-87.86296,"ath_date":"2018-01-07T00:00:00.000Z","atl":0.00268621,"atl_change_percentage":15255.16102,"atl_date":"2014-05-22T00:00:00.000Z","roi":null,"last_updated":"2023-01-26T07:47:51.640Z"},{"id":"binance-usd","symbol":"busd","name":"Binance USD","image":"https://assets.coingecko.com/coins/images/9576/large/BUSD.png?1568947766","current_price":1.002,"market_cap":15594117007,"market_cap_rank":7,"fully_diluted_valuation":15594117007,"total_volume":12399209907,"high_24h":1.006,"low_24h":0.978392,"price_change_24h":0.0022793,"price_change_percentage_24h":0.22795,"market_cap_change_24h":213528691,"market_cap_change_percentage_24h":1.3883,"circulating_supply":15578970702.39,"total_supply":15578970702.39,"max_supply":null,"ath":1.15,"ath_change_percentage":-13.27326,"ath_date":"2020-03-13T02:35:42.953Z","atl":0.901127,"atl_change_percentage":11.08349,"atl_date":"2021-05-19T13:04:37.445Z","roi":null,"last_updated":"2023-01-26T07:47:45.147Z"},{"id":"cardano","symbol":"ada","name":"Cardano","image":"https://assets.coingecko.com/coins/images/975/large/cardano.png?1547034860","current_price":0.377826,"market_cap":13234695762,"market_cap_rank":8,"fully_diluted_valuation":16994177637,"total_volume":441587103,"high_24h":0.382793,"low_24h":0.35458,"price_change_24h":0.0185917,"price_change_percentage_24h":5.17537,"market_cap_change_24h":657358040,"market_cap_change_percentage_24h":5.22653,"circulating_supply":35045020830.3234,"total_supply":45000000000.0,"max_supply":45000000000.0,"ath":3.09,"ath_change_percentage":-87.76614,"ath_date":"2021-09-02T06:00:10.474Z","atl":0.01925275,"atl_change_percentage":1861.52992,"atl_date":"2020-03-13T02:22:55.044Z","roi":null,"last_updated":"2023-01-26T07:47:42.416Z"},{"id":"dogecoin","symbol":"doge","name":"Dogecoin","image":"https://assets.coingecko.com/coins/images/5/large/dogecoin.png?1547792256","current_price":0.086519,"market_cap":11953535240,"market_cap_rank":9,"fully_diluted_valuation":null,"total_volume":676891358,"high_24h":0.087969,"low_24h":0.083301,"price_change_24h":0.00155793,"price_change_percentage_24h":1.83369,"market_cap_change_24h":230661499,"market_cap_change_percentage_24h":1.96762,"circulating_supply":137877966383.705,"total_supply":null,"max_supply":null,"ath":0.731578,"ath_change_percentage":-88.14939,"ath_date":"2021-05-08T05:08:23.458Z","atl":8.69e-05,"atl_change_percentage":99661.4938,"atl_date":"2015-05-06T00:00:00.000Z","roi":null,"last_updated":"2023-01-26T07:47:27.695Z"},{"id":"solana","symbol":"sol","name":"Solana","image":"https://assets.coingecko.com/coins/images/4128/large/solana.png?1640133422","current_price":24.44,"market_cap":9103635900,"market_cap_rank":10,"fully_diluted_valuation":13221546992,"total_volume":1195647392,"high_24h":25.11,"low_24h":23.01,"price_change_24h":1.11,"price_change_percentage_24h":4.76536,"market_cap_change_24h":431667173,"market_cap_change_percentage_24h":4.97773,"circulating_supply":371340835.953211,"total_supply":539312025.077523,"max_supply":null,"ath":259.96,"ath_change_percentage":-90.58081,"ath_date":"2021-11-06T21:54:35.825Z","atl":0.500801,"atl_change_percentage":4789.37505,"atl_date":"2020-05-11T19:35:23.449Z","roi":null,"last_updated":"2023-01-26T07:47:44.171Z"}]
+
+
+
+fetch("products.json")
+.then(function(response){
+    return response.json();
+})
+.then(function(products){
+    let placeholder = document.querySelector("#data-output");
+    let out = "";
+    for(let product of products){
+        out += `
+            <tr>
+            <td> <img src='${product.image}'> </td>
+            <td>${product.name}</td>
+            <td>${product.price}</td>
+            <td>${product.inventory}</td>
+            <td>${product.productCode}</td>
+            </tr>
+        `;
+    }
+    
+    placeholder.innerHTML = out;
+});
